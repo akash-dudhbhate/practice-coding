85d47ef988cdf82a0bf643e277e92259023ed47e
Utkarsha <utkarshanarsale76@gmail.com>
2026-09-17 12:40:16 +0530

    use modulues operator % to check no is even or add

--- python/lesson-01-variables-types-functions/easy/p01-max-of-two.py
+++ python/lesson-01-variables-types-functions/easy/p01-max-of-two.py
@@ -6,7 +6,12 @@ CONCEPT: Variables, types, functions, conditionals.
 
 # See concepts.md in this lesson folder for detailed explanations (WHY/WHERE/WHAT-GOES-WRONG).
 
-# TODO: Write your complete solution from scratch below (function signature + body).
-#       Remove this TODO line when done.
 
 """
+def max_of_two(a,b):
+ if a>b:
+  return a 
+ else :
+  return b
+print(max_of_two(50,20))
+print(max_of_two(90,75))
--- python/lesson-01-variables-types-functions/easy/p02-even-or-odd.py
+++ python/lesson-01-variables-types-functions/easy/p02-even-or-odd.py
@@ -6,6 +6,12 @@ CONCEPT: Variables, types, functions, modulo operator.
 
 # See concepts.md in this lesson folder for detailed explanations (WHY/WHERE/WHAT-GOES-WRONG).
 
-# TODO: Write your complete solution from scratch below (function signature + body).
-#       Remove this TODO line when done.
+   
 """
+def even_or_odd(num):
+ if num% 2==0 :
+  return "even"
+ else :
+  return "odd"
+print(even_or_odd(10))
+print(even_or_odd(7))
